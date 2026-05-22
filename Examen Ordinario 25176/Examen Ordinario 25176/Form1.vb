42e1e3c692f3a354763c781a79aaa391a18aad0a
alberto-lomas <alberto.lomas@iest.edu.mx>
2026-05-22 09:43:38 -0600

    Add files via upload

--- Examen Ordinario 25176/Examen Ordinario 25176/Form1.vb
+++ Examen Ordinario 25176/Examen Ordinario 25176/Form1.vb
@@ -1,14 +1,14 @@
 ﻿Public Class Form1
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
         Dim cantidad As Integer
-        cantidad = Val(TextBox2.Text)
+        cantidad = TextBox2.Text
         cantidad = cantidad + 1
         TextBox2.Text = cantidad
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         Dim cantidad As Integer
-        cantidad = Val(TextBox2.Text)
+        cantidad = TextBox2.Text
         If cantidad > 0 Then
             cantidad = cantidad - 1
             TextBox2.Text = cantidad
@@ -19,8 +19,8 @@
         Dim precio As Integer
         Dim cantidad As Integer
         Dim subt As Integer
-        precio = Val(TextBox1.Text)
-        cantidad = Val(TextBox2.Text)
+        precio = TextBox1.Text
+        cantidad = TextBox2.Text
         subt = precio * cantidad
         subtotal.Text = subt.ToString()
     End Sub
@@ -28,7 +28,7 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim subt As Integer
         Dim iva As Integer
-        subt = Val(subtotal.Text)
+        subt = subtotal.Text
         iva = subt * 0.16
         impuestos.Text = iva.ToString()
     End Sub
@@ -37,8 +37,8 @@
         Dim subt As Integer
         Dim iva As Integer
         Dim tot As Integer
-        iva = Val(impuestos.Text)
-        subt = Val(subtotal.Text)
+        iva = impuestos.Text
+        subt = subtotal.Text
         tot = iva + subt
         total.Text = tot.ToString()
     End Sub
