1702005dfead83713c6b353281fa9ca5801b068c
alberto-lomas <alberto.lomas@iest.edu.mx>
2026-05-22 09:35:58 -0600

    Add files via upload

---
+++ Examen Ordinario 25176/Examen Ordinario 25176/Form1.Designer.vb
@@ -0,0 +1,231 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.subtotal = New System.Windows.Forms.Label()
+        Me.impuestos = New System.Windows.Forms.Label()
+        Me.total = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(117, 61)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "0"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(152, 111)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.Text = "0"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'subtotal
+        '
+        Me.subtotal.AutoSize = True
+        Me.subtotal.Location = New System.Drawing.Point(265, 169)
+        Me.subtotal.Name = "subtotal"
+        Me.subtotal.Size = New System.Drawing.Size(13, 13)
+        Me.subtotal.TabIndex = 2
+        Me.subtotal.Text = "0"
+        '
+        'impuestos
+        '
+        Me.impuestos.AutoSize = True
+        Me.impuestos.Location = New System.Drawing.Point(265, 206)
+        Me.impuestos.Name = "impuestos"
+        Me.impuestos.Size = New System.Drawing.Size(13, 13)
+        Me.impuestos.TabIndex = 3
+        Me.impuestos.Text = "0"
+        '
+        'total
+        '
+        Me.total.AutoSize = True
+        Me.total.Location = New System.Drawing.Point(265, 242)
+        Me.total.Name = "total"
+        Me.total.Size = New System.Drawing.Size(13, 13)
+        Me.total.TabIndex = 4
+        Me.total.Text = "0"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(95, 164)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(97, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Calcular Subtotal"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(95, 201)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(97, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Calcular IVA"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(95, 237)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(97, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Calcular Total"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(56, 64)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Precio       $"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(117, 109)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(29, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "-1"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(258, 109)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(29, 23)
+        Me.Button5.TabIndex = 10
+        Me.Button5.Text = "+1"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(56, 114)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(52, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Cantidad:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(198, 169)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Subtotal:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(198, 206)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "IVA (16%):"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(198, 242)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Total:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(127, 20)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(127, 13)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Aplicación de Tienda"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(377, 323)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.total)
+        Me.Controls.Add(Me.impuestos)
+        Me.Controls.Add(Me.subtotal)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents subtotal As Label
+    Friend WithEvents impuestos As Label
+    Friend WithEvents total As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Examen Ordinario 25176/Examen Ordinario 25176/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim cantidad As Integer
+        cantidad = Val(TextBox2.Text)
+        cantidad = cantidad + 1
+        TextBox2.Text = cantidad
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim cantidad As Integer
+        cantidad = Val(TextBox2.Text)
+        If cantidad > 0 Then
+            cantidad = cantidad - 1
+            TextBox2.Text = cantidad
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim precio As Integer
+        Dim cantidad As Integer
+        Dim subt As Integer
+        precio = Val(TextBox1.Text)
+        cantidad = Val(TextBox2.Text)
+        subt = precio * cantidad
+        subtotal.Text = subt.ToString()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim subt As Integer
+        Dim iva As Integer
+        subt = Val(subtotal.Text)
+        iva = subt * 0.16
+        impuestos.Text = iva.ToString()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim subt As Integer
+        Dim iva As Integer
+        Dim tot As Integer
+        iva = Val(impuestos.Text)
+        subt = Val(subtotal.Text)
+        tot = iva + subt
+        total.Text = tot.ToString()
+    End Sub
+End Class
